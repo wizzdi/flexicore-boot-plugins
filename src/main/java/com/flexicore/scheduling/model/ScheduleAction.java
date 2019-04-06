31f0cedff313ee3b5916a34262ab625ad53ce5bf
asaf <asaf@flexi-core.com>
2019-04-07 01:46:56 +0300

    1.1.2 schedule

--- src/main/java/com/flexicore/scheduling/model/ScheduleAction.java
+++ src/main/java/com/flexicore/scheduling/model/ScheduleAction.java
@@ -2,6 +2,7 @@ package com.flexicore.scheduling.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.flexicore.model.Baseclass;
+import com.flexicore.model.dynamic.DynamicExecution;
 import com.flexicore.model.dynamic.ExecutionParametersHolder;
 
 import javax.persistence.CascadeType;
@@ -12,20 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class ScheduleAction extends Baseclass {
+public class ScheduleAction extends DynamicExecution {
     private static ScheduleAction s_Singleton=new ScheduleAction();
     public  static ScheduleAction s() {return s_Singleton;}
-
 
     @JsonIgnore
     @OneToMany(targetEntity = ScheduleToAction.class,mappedBy = "rightside",cascade = {CascadeType.PERSIST,CascadeType.MERGE})
     private List<ScheduleToAction> scheduleToActions=new ArrayList<>();
-    @OneToMany(targetEntity = ServiceCanonicalName.class,mappedBy = "scheduleAction",cascade = {CascadeType.PERSIST,CascadeType.MERGE})
-    private List<ServiceCanonicalName> serviceCanonicalNames=new ArrayList<>();
-    private String methodName;
-
-    @OneToOne(targetEntity = ExecutionParametersHolder.class)
-    private ExecutionParametersHolder executionParametersHolder;
 
     @JsonIgnore
     @OneToMany(targetEntity = ScheduleToAction.class,mappedBy = "rightside",cascade = {CascadeType.PERSIST,CascadeType.MERGE})
@@ -35,37 +29,6 @@ public class ScheduleAction extends Baseclass {
 
     public ScheduleAction setScheduleToActions(List<ScheduleToAction> scheduleToActions) {
         this.scheduleToActions = scheduleToActions;
-        return this;
-    }
-
-
-    @OneToMany(targetEntity = ServiceCanonicalName.class,mappedBy = "scheduleAction",cascade = {CascadeType.PERSIST,CascadeType.MERGE})
-    public List<ServiceCanonicalName> getServiceCanonicalNames() {
-        return serviceCanonicalNames;
-    }
-
-    public ScheduleAction setServiceCanonicalNames(List<ServiceCanonicalName> serviceCanonicalNames) {
-        this.serviceCanonicalNames = serviceCanonicalNames;
-        return this;
-    }
-
-    public String getMethodName() {
-        return methodName;
-    }
-
-    public ScheduleAction setMethodName(String methodName) {
-        this.methodName = methodName;
-        return this;
-    }
-
-
-    @OneToOne(targetEntity = ExecutionParametersHolder.class)
-    public ExecutionParametersHolder getExecutionParametersHolder() {
-        return executionParametersHolder;
-    }
-
-    public ScheduleAction setExecutionParametersHolder(ExecutionParametersHolder executionParametersHolder) {
-        this.executionParametersHolder = executionParametersHolder;
         return this;
     }
 }
