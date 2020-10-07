7f97121827e2631a62cf80bd8fce00ec6e4531bb
asaf <asaf@flexi-core.com>
2020-10-07 15:08:06 +0300

    init

---
+++ src/main/java/com/flexicore/ui/component/model/UIComponent.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ *  Copyright (C) FlexiCore, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by Avishay Ben Natan And Asaf Ben Natan, October 2015
+ ******************************************************************************/
+package com.flexicore.ui.component.model;
+
+
+import com.flexicore.model.Baseclass;
+import com.flexicore.security.SecurityContext;
+
+import javax.persistence.Entity;
+
+@Entity
+public class UIComponent extends Baseclass {
+
+
+	public UIComponent() {
+	}
+
+	public UIComponent(String name, SecurityContext securityContext) {
+		super(name, securityContext);
+	}
+
+	private String externalId;
+
+
+	public String getExternalId() {
+		return externalId;
+	}
+
+	public void setExternalId(String externalId) {
+		this.externalId = externalId;
+	}
+}
