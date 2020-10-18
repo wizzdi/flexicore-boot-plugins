3344c6eeca30dd7aa518cc2f2f90c78bd54bdf87
asaf <asaf@flexi-core.com>
2020-10-18 15:27:44 +0300

    3.1.4 - fc 4.2.0 compatibility

--- src/main/java/com/flexicore/ui/model/Preset.java
+++ src/main/java/com/flexicore/ui/model/Preset.java
@@ -2,19 +2,16 @@ package com.flexicore.ui.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.flexicore.model.Baseclass;
-import com.flexicore.model.Presenter;
-import com.flexicore.model.dynamic.DynamicExecution;
 import com.flexicore.security.SecurityContext;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Preset extends Presenter {
+public class Preset extends Baseclass {
 
 	private String externalId;
 
