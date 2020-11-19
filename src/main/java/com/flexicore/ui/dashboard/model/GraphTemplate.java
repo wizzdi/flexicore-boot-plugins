f2ab3e61967e3da08e43b2f51e06b1125c854027
asaf <asaf@flexi-core.com>
2020-11-19 16:24:11 +0200

    1.1.0 - added graph template

--- src/main/java/com/flexicore/ui/dashboard/model/GraphTemplate.java
+++ src/main/java/com/flexicore/ui/dashboard/model/GraphTemplate.java
@@ -2,6 +2,7 @@ package com.flexicore.ui.dashboard.model;
 
 import com.flexicore.model.Baseclass;
 import com.flexicore.model.FileResource;
+import com.flexicore.security.SecurityContext;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -11,6 +12,13 @@ public class GraphTemplate extends Baseclass {
 
     @ManyToOne(targetEntity = FileResource.class)
     private FileResource fileResource;
+
+    public GraphTemplate() {
+    }
+
+    public GraphTemplate(String name, SecurityContext securityContext) {
+        super(name, securityContext);
+    }
 
     @ManyToOne(targetEntity = FileResource.class)
     public FileResource getFileResource() {
