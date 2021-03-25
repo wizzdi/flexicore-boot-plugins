26a2ad13660c26dd69a6f945a12086af16e572f9
asaf <asaf@flexi-core.com>
2021-03-25 15:27:19 +0200

    init

---
+++ src/test/java/com/wizzdi/flexicore/encryption/EncryptionServiceTest.java
@@ -0,0 +1,39 @@
+package com.wizzdi.flexicore.encryption;
+
+import com.wizzdi.flexicore.encryption.app.App;
+import com.wizzdi.flexicore.encryption.service.EncryptionService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.security.GeneralSecurityException;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = App.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+
+public class EncryptionServiceTest {
+
+
+    @Autowired
+    private EncryptionService encryptionService;
+
+
+    @Test
+    @Order(0)
+    public void testEncrypt() throws GeneralSecurityException {
+        String plainText="test";
+        byte[] associatedData = "key".getBytes();
+        byte[] encrypted = encryptionService.encrypt(plainText.getBytes(), associatedData);
+        byte[] decrypted = encryptionService.decrypt(encrypted, associatedData);
+        String output=new String(decrypted);
+        Assertions.assertEquals(plainText,output);
+
+    }
+
+}
