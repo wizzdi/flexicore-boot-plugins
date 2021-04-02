b76b09fb218e9d4f8394a78e91a51d032fd6d20c
asaf <asaf@flexi-core.com>
2021-04-02 22:00:45 +0300

    2.0.1 - updated dependencies

--- src/test/java/com/flexicore/ui/component/rest/EntitiesSeedProvider.java
+++ src/test/java/com/flexicore/ui/component/rest/EntitiesSeedProvider.java
@@ -1,8 +1,8 @@
 package com.flexicore.ui.component.rest;
 
 import com.flexicore.model.Baseclass;
-import com.flexicore.test.EntityProviderClasses;
 import com.flexicore.ui.component.model.UIComponent;
+import com.wizzdi.flexicore.boot.jpa.service.EntitiesHolder;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +18,8 @@ public class EntitiesSeedProvider {
     @Primary
     @Bean
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
-    public EntityProviderClasses getSeeds() {
-        return new EntityProviderClasses(new HashSet<>(Arrays.asList(Baseclass.class, UIComponent.class)));
+    public EntitiesHolder getSeeds() {
+        return new EntitiesHolder(new HashSet<>(Arrays.asList(Baseclass.class, UIComponent.class)));
 
     }
 }
