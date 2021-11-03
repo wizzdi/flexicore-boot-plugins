1bd76a0cd58b14ceebda7764c1ceda1ef210162e
Asaf Ben Natan <asaf@wizzdi.com>
2021-11-03 09:57:28 +0200

    added approving and gateway user

--- src/main/java/com/wizzdi/basic/iot/model/Gateway.java
+++ src/main/java/com/wizzdi/basic/iot/model/Gateway.java
@@ -1,13 +1,24 @@
 package com.wizzdi.basic.iot.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flexicore.model.SecurityUser;
+
 import javax.persistence.Entity;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Gateway extends Remote {
 
     @Lob
     private String publicKey;
+
+    @JsonIgnore
+    @ManyToOne(targetEntity = SecurityUser.class)
+    private SecurityUser approvingUser;
+    @JsonIgnore
+    @ManyToOne(targetEntity = SecurityUser.class)
+    private SecurityUser gatewayUser;
 
 
     @Lob
@@ -17,6 +28,29 @@ public class Gateway extends Remote {
 
     public <T extends Gateway> T setPublicKey(String publicKey) {
         this.publicKey = publicKey;
+        return (T) this;
+    }
+
+
+    @JsonIgnore
+    @ManyToOne(targetEntity = SecurityUser.class)
+    public SecurityUser getApprovingUser() {
+        return approvingUser;
+    }
+
+    public <T extends Gateway> T setApprovingUser(SecurityUser approvingUser) {
+        this.approvingUser = approvingUser;
+        return (T) this;
+    }
+
+    @JsonIgnore
+    @ManyToOne(targetEntity = SecurityUser.class)
+    public SecurityUser getGatewayUser() {
+        return gatewayUser;
+    }
+
+    public <T extends Gateway> T setGatewayUser(SecurityUser gatewayUser) {
+        this.gatewayUser = gatewayUser;
         return (T) this;
     }
 }
