a57c5ea733a1cf6dc3ef91fdcbb6460e92ba9fbe
Asaf Ben Natan <asaf@wizzdi.com>
2021-11-15 14:52:38 +0200

    init

---
+++ src/main/java/com/wizzdi/video/conference/model/Room.java
@@ -0,0 +1,27 @@
+package com.wizzdi.video.conference.model;
+
+import com.flexicore.model.SecuredBasic;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Room extends SecuredBasic {
+
+  @ManyToOne(targetEntity = VideoServer.class)
+  private VideoServer videoServer;
+
+  /** @return videoServer */
+  @ManyToOne(targetEntity = VideoServer.class)
+  public VideoServer getVideoServer() {
+    return this.videoServer;
+  }
+
+  /**
+   * @param videoServer videoServer to set
+   * @return Room
+   */
+  public <T extends Room> T setVideoServer(VideoServer videoServer) {
+    this.videoServer = videoServer;
+    return (T) this;
+  }
+}
---
+++ src/main/java/com/wizzdi/video/conference/model/VideoServerUser.java
@@ -0,0 +1,7 @@
+package com.wizzdi.video.conference.model;
+
+import com.flexicore.model.SecuredBasic;
+import javax.persistence.Entity;
+
+@Entity
+public class VideoServerUser extends SecuredBasic {}
