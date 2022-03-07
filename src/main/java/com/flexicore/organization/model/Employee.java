6117d0984da0c6075c3dce89cd8c5abd00cf0ccc
Avishay Ben Natan <avishay@flexi-core.com>
2022-03-07 13:31:15 +0200

    added externalId to Organization and to Employee
    added indexes for the above

--- src/main/java/com/flexicore/organization/model/Employee.java
+++ src/main/java/com/flexicore/organization/model/Employee.java
@@ -3,15 +3,20 @@ package com.flexicore.organization.model;
 import com.flexicore.model.SecuredBasic;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Table(name = "employee", indexes = {@Index(name = "externalid_ix", columnList = "externalid", unique = false)
+,@Index(name = "externalId2_ix", columnList = "externalid2", unique = false)})
 @Entity
 public class Employee extends SecuredBasic {
 
-
+	private String externalId;
+	private String externalId2;
 	@ManyToOne(targetEntity = Organization.class)
 	private Organization organization;
-
+	boolean organizationAdmin;
 	@ManyToOne(targetEntity = Organization.class)
 	public Organization getOrganization() {
 		return organization;
@@ -19,6 +24,32 @@ public class Employee extends SecuredBasic {
 
 	public Employee setOrganization(Organization organization) {
 		this.organization = organization;
+		return this;
+	}
+
+	public String getExternalId() {
+		return externalId;
+	}
+
+	public Employee setExternalId(String externalId) {
+		this.externalId = externalId;
+		return this;
+	}
+	public boolean isOrganizationAdmin() {
+		return organizationAdmin;
+	}
+
+	public Employee setOrganizationAdmin(boolean organizationAdmin) {
+		this.organizationAdmin = organizationAdmin;
+		return this;
+	}
+
+	public String getExternalId2() {
+		return externalId2;
+	}
+
+	public Employee setExternalId2(String internalId) {
+		this.externalId2 = internalId;
 		return this;
 	}
 }
--- src/main/java/com/flexicore/organization/model/Organization.java
+++ src/main/java/com/flexicore/organization/model/Organization.java
@@ -1,23 +1,18 @@
 package com.flexicore.organization.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.flexicore.model.Baseclass;
+import com.flexicore.model.PermissionGroup;
 import com.flexicore.model.SecuredBasic;
 import com.flexicore.model.territories.Address;
-import com.flexicore.security.SecurityContextBase;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import java.lang.annotation.Target;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Table(name = "organization", indexes = {@Index(name = "externalid_ix", columnList = "externalid", unique = false)})
 @Entity
 public class Organization extends SecuredBasic {
 
-
+	private String externalId;
 	@ManyToOne(targetEntity = Address.class)
 	private Address mainAddress;
 
@@ -39,7 +34,8 @@ public class Organization extends SecuredBasic {
 		this.branches = branches;
 		return this;
 	}
-
+	@OneToOne(targetEntity = PermissionGroup.class)
+	private PermissionGroup permissionGroup;
 	@OneToMany(targetEntity = Employee.class, mappedBy = "organization")
 	@JsonIgnore
 	public List<Employee> getEmployees() {
@@ -59,5 +55,23 @@ public class Organization extends SecuredBasic {
 	public <T extends Organization> T setMainAddress(Address mainAddress) {
 		this.mainAddress = mainAddress;
 		return (T) this;
+	}
+
+	public String getExternalId() {
+		return externalId;
+	}
+
+	public Organization setExternalId(String externalId) {
+		this.externalId = externalId;
+		return this;
+	}
+
+	public PermissionGroup getPermissionGroup() {
+		return permissionGroup;
+	}
+
+	public Organization setPermissionGroup(PermissionGroup permissionGroup) {
+		this.permissionGroup = permissionGroup;
+		return this;
 	}
 }
