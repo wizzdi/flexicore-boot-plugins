e7c2e6154f2e566d2f149cdf2c325e98f8731399
Asaf Ben Natan <asaf@wizzdi.com>
2023-01-03 14:57:23 +0200

    added default device type for mapped poi

--- src/main/java/com/wizzdi/basic/iot/model/DeviceType.java
+++ src/main/java/com/wizzdi/basic/iot/model/DeviceType.java
@@ -1,9 +1,11 @@
 package com.wizzdi.basic.iot.model;
 
 import com.flexicore.model.SecuredBasic;
+import com.wizzdi.maps.model.MapIcon;
 
 import javax.persistence.Entity;
 import javax.persistence.Index;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -11,4 +13,17 @@ import javax.persistence.Table;
         @Index(name = "device_type_idx",columnList = "name")
 })
 public class DeviceType extends SecuredBasic {
+
+    @ManyToOne(targetEntity = MapIcon.class)
+    private MapIcon defaultMapIcon;
+
+    @ManyToOne(targetEntity = MapIcon.class)
+    public MapIcon getDefaultMapIcon() {
+        return defaultMapIcon;
+    }
+
+    public <T extends DeviceType> T setDefaultMapIcon(MapIcon defaultMapIcon) {
+        this.defaultMapIcon = defaultMapIcon;
+        return (T) this;
+    }
 }
