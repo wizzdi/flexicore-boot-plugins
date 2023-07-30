8765b66b1f31632ad0a4c88a501f25db2fd730b7
Asaf <asaf@flexi-core.com>
2023-07-30 16:25:26 +0300

    Added ui-component-model files to the monorepo

---
+++ ui-component-model/src/main/java/com/flexicore/ui/component/model/UIComponent.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *  Copyright (C) FlexiCore, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by Avishay Ben Natan And Asaf Ben Natan, October 2015
+ ******************************************************************************/
+package com.flexicore.ui.component.model;
+
+
+import com.flexicore.model.SecuredBasic;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class UIComponent extends SecuredBasic {
+
+
+
+	private String externalId;
+
+
+	public String getExternalId() {
+		return externalId;
+	}
+
+	public void setExternalId(String externalId) {
+		this.externalId = externalId;
+	}
+}
