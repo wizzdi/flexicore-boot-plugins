c707312d1a856eeb334b1c4a47f5bf323e1f29aa
Asaf <asaf@flexi-core.com>
2023-07-30 16:29:24 +0300

    Added video-conference-model files to the monorepo

---
+++ video-conference-model/src/main/java/com/wizzdi/video/conference/model/RoomToVideoServerUser.java
@@ -0,0 +1,61 @@
+package com.wizzdi.video.conference.model;
+
+import com.flexicore.model.SecuredBasic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class RoomToVideoServerUser extends SecuredBasic {
+
+  @ManyToOne(targetEntity = Room.class)
+  private Room room;
+
+  private String roomUrl;
+
+  @ManyToOne(targetEntity = VideoServerUser.class)
+  private VideoServerUser videoServerUser;
+
+  /** @return room */
+  @ManyToOne(targetEntity = Room.class)
+  public Room getRoom() {
+    return this.room;
+  }
+
+  /**
+   * @param room room to set
+   * @return RoomToVideoServerUser
+   */
+  public <T extends RoomToVideoServerUser> T setRoom(Room room) {
+    this.room = room;
+    return (T) this;
+  }
+
+  /** @return roomUrl */
+  public String getRoomUrl() {
+    return this.roomUrl;
+  }
+
+  /**
+   * @param roomUrl roomUrl to set
+   * @return RoomToVideoServerUser
+   */
+  public <T extends RoomToVideoServerUser> T setRoomUrl(String roomUrl) {
+    this.roomUrl = roomUrl;
+    return (T) this;
+  }
+
+  /** @return videoServerUser */
+  @ManyToOne(targetEntity = VideoServerUser.class)
+  public VideoServerUser getVideoServerUser() {
+    return this.videoServerUser;
+  }
+
+  /**
+   * @param videoServerUser videoServerUser to set
+   * @return RoomToVideoServerUser
+   */
+  public <T extends RoomToVideoServerUser> T setVideoServerUser(VideoServerUser videoServerUser) {
+    this.videoServerUser = videoServerUser;
+    return (T) this;
+  }
+}
---
+++ video-conference-model/src/main/java/com/wizzdi/video/conference/model/VideoServer.java
@@ -0,0 +1,24 @@
+package com.wizzdi.video.conference.model;
+
+import com.flexicore.model.SecuredBasic;
+import jakarta.persistence.Entity;
+
+@Entity
+public class VideoServer extends SecuredBasic {
+
+  private String baseUrl;
+
+  /** @return baseUrl */
+  public String getBaseUrl() {
+    return this.baseUrl;
+  }
+
+  /**
+   * @param baseUrl baseUrl to set
+   * @return VideoServer
+   */
+  public <T extends VideoServer> T setBaseUrl(String baseUrl) {
+    this.baseUrl = baseUrl;
+    return (T) this;
+  }
+}
