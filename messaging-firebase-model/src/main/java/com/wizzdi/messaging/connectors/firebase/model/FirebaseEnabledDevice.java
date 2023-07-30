7146475854a458b7ff32823edf9dd3ee3525b52c
Asaf <asaf@flexi-core.com>
2023-07-30 16:12:34 +0300

    Added messaging-firebase-model files to the monorepo

---
+++ messaging-firebase-model/src/main/java/com/wizzdi/messaging/connectors/firebase/model/FirebaseEnabledDevice.java
@@ -0,0 +1,11 @@
+package com.wizzdi.messaging.connectors.firebase.model;
+
+import com.wizzdi.messaging.model.MessageReceiverDevice;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class FirebaseEnabledDevice extends MessageReceiverDevice {
+
+
+}
